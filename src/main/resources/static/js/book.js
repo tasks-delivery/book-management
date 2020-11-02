982c68662d8b0290dbd066a934643fe6d9ea7904
Fiodar <f.motin@sam-solutions.com>
2020-11-03 00:36:49 +0300

    added validations

--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -5,10 +5,20 @@
 	var categoryList = [];
     var selector = document.querySelector("select");
     var option = selector.options[selector.selectedIndex];
+    var bookName = $("#book-name").val();
+
+    if(option.value.length == 0){
+        throw new Error('Category or book name are empty');
+    }
+
+    if(bookName.length == 0){
+        throw new Error('Category or book name are empty');
+    }
+
     categoryList.push(option.value);
 
 	var bookData = {
-            name: $("#book-name").val(),
+            name: bookName,
             categories:categoryList,
             user:
                 {
@@ -18,20 +28,27 @@
         }
 
     console.log(bookData);
-
     $.ajax({
-      method: "POST",
-      url: "book",
-      dataType: "json",
-      contentType: 'application/json',
-      data: JSON.stringify(bookData),
-      success: function(data) {
-        console.log(data);
-      },
-      error: function(er) {
-        console.log(er);
-      }
-    });
+         method: "POST",
+         url: "book",
+         dataType: "json",
+         contentType: 'application/json',
+         data: JSON.stringify(bookData),
+         success: function(data) {
+           console.log(data);
+         },
+         error: function(er) {
+           console.log(er);
+         },
+          statusCode: {
+             200: function() {
+               location.href='/search';
+             },
+             400: function() {
+               throw new Error('Book already exists');
+             }
+           }
+       });
 
   })
 });
