23d527d4a9f2f51f43ade48ab33234c901943206
Fiodar <f.motin@sam-solutions.com>
2020-11-04 00:11:08 +0300

    adde back-end side

--- src/main/java/book/platform/controller/BookController.java
+++ src/main/java/book/platform/controller/BookController.java
@@ -13,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,6 +33,11 @@ public class BookController {
         Book book = (Book)JsonUtil.jsonToObject(body, Book.class);
         Boolean duplicate = isDuplicate(book.getName(), book.getCategories());
         if (!duplicate){
+            if (book.getUser().getFirstName().isEmpty() || book.getUser().getLastName().isEmpty()){
+                book.setAvailable(true);
+            }else {
+                book.setAvailable(false);
+            }
             bookRepository.save(book);
             return ResponseEntity.ok(HttpStatus.OK);
         }else {
@@ -40,17 +47,42 @@ public class BookController {
     }
 
     private Boolean isDuplicate(String name, List<String> categories){
-        List<Book> books = new ArrayList<Book>();
-        bookRepository.findAll().iterator().forEachRemaining(books::add);
-
-        books = books.stream().filter(i -> i.getCategories().containsAll(categories))
-            .filter(i -> i.getName().equals(name)).collect(Collectors.toList());
+        List<Book> books = convertBookToList(bookRepository.findAll()).stream()
+            .filter(i -> i.getCategories().containsAll(categories))
+            .filter(i -> i.getName().equals(name))
+            .collect(Collectors.toList());
         return books.size() != 0;
     }
 
+    private List<Book> convertBookToList(Iterable<Book> bookIterable){
+        List<Book> books = new ArrayList<>();
+        bookIterable.iterator().forEachRemaining(books::add);
+        return books;
+    }
+
     @GetMapping("/books")
-    public Iterable<Book> getBooks() {
+    public Iterable<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    @GetMapping("/books/{name}")
+    public List<Book> getBooksByName(@RequestParam("categories") List<String> categories, @PathVariable String name) {
+        List<Book> books = convertBookToList(bookRepository.findAll());
+        if (!categories.isEmpty()){
+            if (!categories.get(0).isEmpty()){
+                books = books.stream()
+                    .filter(i -> i.getCategories().containsAll(categories))
+                    .collect(Collectors.toList());
+            }
+        }
+
+        if (!name.isEmpty()){
+            books = books.stream()
+                .filter(i -> i.getName().contains(name))
+                .collect(Collectors.toList());
+        }
+
+        return books;
     }
 
 }
