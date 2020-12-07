f5fd0e8a23f965c11edb6f197b8d06c01c1a2bd3
Fiodar <f.motin@sam-solutions.com>
2020-12-07 22:26:59 +0300

    added book type

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -68,6 +68,12 @@ $(document).ready(function() {
                 number:'2'
                 },
 
+//TODO: Fake book location data, should be replaced to final solution in future.
+            bookTypes:
+                [{
+                name:'Free book'
+                }],
+
 //TODO: Fake publisher data, should be replaced to final solution in future.
             publisher:
                 {
